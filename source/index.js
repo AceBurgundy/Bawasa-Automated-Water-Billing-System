7b648ea779a0bb113757a3e9198739462ca9153c
AceBurgundy <Samadriansabalo99@gmail.com>
2023-07-16 17:14:04 +0800

    Changed back to index.html

--- source/index.js
+++ source/index.js
@@ -25,7 +25,7 @@ const createWindow = () => {
     icon
   });
 
-  mainWindow.loadFile(path.join(__dirname, 'Pages/Authentication/templates/authentication.html'));
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   mainWindow.webContents.openDevTools();
 };
