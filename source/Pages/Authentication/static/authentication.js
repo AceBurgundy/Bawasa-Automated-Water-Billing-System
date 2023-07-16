9290084b09f605fac94d1178d73e2be6e14fbea0
AceBurgundy <Samadriansabalo99@gmail.com>
2023-07-16 17:14:57 +0800

    Added appendToHead for style on DOMContentLoaded.

--- source/Pages/Authentication/static/authentication.js
+++ source/Pages/Authentication/static/authentication.js
@@ -1,8 +1,12 @@
 import loadLogin from "./login.js"
 import loadRegister from "./register.js"
-import { transition } from "../../../helper.js"
+import { transition, appendToHead } from "../../../helper.js"
 
-document.addEventListener("DOMContentLoaded", loadLogin())
+document.addEventListener("DOMContentLoaded", () => {
+    const authenticationStyle = `<link rel="stylesheet" href="Pages/Authentication/static/authentication.css">`;
+    appendToHead(authenticationStyle)
+    loadLogin()
+})
 
 window.onclick = event => {
 
