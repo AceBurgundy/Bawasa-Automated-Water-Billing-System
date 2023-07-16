ba8c76f06730c99f048e04fcf485eaa25594eb96
AceBurgundy <Samadriansabalo99@gmail.com>
2023-07-16 12:41:19 +0800

    Added index.js

---
+++ source/index.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+let icon;
+switch (process.platform) {
+  case 'win32': icon = path.resolve(__dirname, 'assets/images', 'Logo.ico'); break;
+  case 'darwin': icon = path.resolve(__dirname, 'assets/images', 'Logo.icns'); break;
+  case 'linux': icon = path.resolve(__dirname, 'assets/images', 'Logo.png'); break;
+}
+
+const createWindow = () => {
+
+  const mainWindow = new BrowserWindow({
+    minWidth: 720,
+    minHeight: 1280,
+    width: 1280,
+    height: 720,
+    // webPreferences: {
+    //   preload: path.join(__dirname, 'preload.js'),
+    // },
+    icon
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'Pages/Authentication/templates/authentication.html'));
+
+  mainWindow.webContents.openDevTools();
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
