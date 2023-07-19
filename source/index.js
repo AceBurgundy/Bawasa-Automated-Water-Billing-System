60d8fc977d7df9bec727bca387090c6c2bf12616
AceBurgundy <Samadriansabalo99@gmail.com>
2023-07-19 21:46:03 +0800

    minor changes and brought back connection to preload.js

--- source/index.js
+++ source/index.js
@@ -1,45 +1,53 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+require("./Pages/Authentication/view")
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
-if (require('electron-squirrel-startup')) {
-  app.quit();
+if (require("electron-squirrel-startup")) {
+    app.quit();
 }
 
 let icon;
+
 switch (process.platform) {
-  case 'win32': icon = path.resolve(__dirname, 'assets/images', 'Logo.ico'); break;
-  case 'darwin': icon = path.resolve(__dirname, 'assets/images', 'Logo.icns'); break;
-  case 'linux': icon = path.resolve(__dirname, 'assets/images', 'Logo.png'); break;
+    case "win32":
+        icon = path.resolve(__dirname, "assets/images", "Logo.ico");
+        break;
+    case "darwin":
+        icon = path.resolve(__dirname, "assets/images", "Logo.icns");
+        break;
+    case "linux":
+        icon = path.resolve(__dirname, "assets/images", "Logo.png");
+        break;
 }
 
 const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        minHeight: 720,
+        minWidth: 1280,
+        height: 720,
+        width: 1280,
+        webPreferences: {
+            contextIsolation: true,
+            nodeIntegration: true,
+            preload: path.join(__dirname, "preload.js"),
+        },
+        icon,
+    });
 
-  const mainWindow = new BrowserWindow({
-    minWidth: 720,
-    minHeight: 1280,
-    width: 1280,
-    height: 720,
-    // webPreferences: {
-    //   preload: path.join(__dirname, 'preload.js'),
-    // },
-    icon
-  });
-
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  mainWindow.webContents.openDevTools();
+    mainWindow.loadFile(path.join(__dirname, "index.html"));
 };
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
 });
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
+
