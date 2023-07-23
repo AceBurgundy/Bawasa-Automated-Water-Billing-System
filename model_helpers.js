9f082b62f97e97bc57d59e13a603e7612144ba0d
AceBurgundy <Samadriansabalo99@gmail.com>
2023-07-23 23:14:30 +0800

    Added model helpers.

---
+++ model_helpers.js
@@ -0,0 +1,47 @@
+const validations = {
+    relationshipOptions: [["Single", "Married", "Rather not say"]],
+    userOptions: [["admin", "meter-reader"]],
+    connectionStatusOptions: [
+        ["Connected", "Due for Disconnection", "Disconnected"],
+    ],
+};
+
+const defaults = {
+    generateNextAccountNumber: async function () {
+        const lastClient = await Client.findOne({
+            order: [["createdAt", "DESC"]],
+        });
+
+        if (!lastClient) {
+            return "0000-AA";
+        }
+
+        let nextNumber = "0000";
+        let nextLetter = "AA";
+
+        const lastAccountNumber = lastClient.account_number;
+        const lastNumberPart = parseInt(lastAccountNumber.slice(0, 4), 10);
+        const lastLetterPart = lastAccountNumber.slice(5);
+
+        if (lastNumberPart === 9999) {
+            nextNumber = "0000";
+
+            const lastLetterCharCode = lastLetterPart.charCodeAt(1);
+
+            lastLetterCharCode === 90
+                ? (nextLetter = "AA")
+                : (nextLetter =
+                      "A" + String.fromCharCode(lastLetterCharCode + 1));
+        } else {
+            nextNumber = String("0000" + (lastNumberPart + 1)).slice(-4);
+            nextLetter = lastLetterPart;
+        }
+
+        return `${nextNumber}-${nextLetter}`;
+    },
+};
+
+module.exports = {
+    validations,
+    defaults,
+};
