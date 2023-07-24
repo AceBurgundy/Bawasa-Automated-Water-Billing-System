c2d4ce5c1ea7aa585b5f3b2bcb653cfdb71fff59
AceBurgundy <Samadriansabalo99@gmail.com>
2023-07-24 22:34:09 +0800

    Minor changes.

--- source/index.js
+++ source/index.js
@@ -1,33 +1,33 @@
 const { app, BrowserWindow } = require("electron");
-require("./Pages/Authentication/view.js");
+require("./pages/authentication/view.js");
 const { resolve, join } = require("path");
-const { db } = require("../sequelize_init")
+// const { db } = require("../sequelize_init")
 
-const Client = require("../models/Client");
-const Client_Address = require("../models/Client_Address");
-const Client_Bill = require("../models/Client_Bill");
-const Client_Connection_Status = require("../models/Client_Connection_Status");
-const Monthly_Reading = require("../models/Monthly_Reading");
-const Partial_Payment = require("../models/Partial_Payment");
-const User_Address = require("../models/User_Address");
-const User_Phone_Number = require("../models/User_Phone_Number");
-const User = require("../models/User");
+// const Client = require("../models/Client");
+// const Client_Address = require("../models/Client_Address");
+// const Client_Bill = require("../models/Client_Bill");
+// const Client_Connection_Status = require("../models/Client_Connection_Status");
+// const Monthly_Reading = require("../models/Monthly_Reading");
+// const Partial_Payment = require("../models/Partial_Payment");
+// const User_Address = require("../models/User_Address");
+// const User_Phone_Number = require("../models/User_Phone_Number");
+// const User = require("../models/User");
 
-async function initializeDatabase() {
-  try {
-    await db.authenticate();
-    console.log("Connection has been established successfully.");
+// async function initializeDatabase() {
+//   try {
+//     await db.authenticate();
+//     console.log("Connection has been established successfully.");
 
-    // Sync all the models to create the tables in the database
-    await db.sync({ force: true });
-    console.log("All models were synchronized successfully.");
+//     // Sync all the models to create the tables in the database
+//     await db.sync({ force: true });
+//     console.log("All models were synchronized successfully.");
 
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-}
+//   } catch (error) {
+//     console.error("Unable to connect to the database:", error);
+//   }
+// }
 
-initializeDatabase();
+// initializeDatabase();
 
 if (require("electron-squirrel-startup")) {
     app.quit();
