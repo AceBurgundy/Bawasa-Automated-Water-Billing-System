91f32fb050d59e7cfbab79d37ac619a7aa4df76b
AceBurgundy <Samadriansabalo99@gmail.com>
2023-07-27 11:00:57 +0800

    Added a custom logger function for debugging.

--- sequelize_init.js
+++ sequelize_init.js
@@ -1,11 +1,21 @@
 const { Sequelize } = require("sequelize") 
 
+function customLogger(query, timing) {
+    console.log("\n\n------------------");
+    console.log("Executing query:", query);
+    if (timing !== undefined) {
+        console.log("Query execution time:", timing, "ms");
+    }
+    console.log("------------------\n\n");
+}
+
 const db = new Sequelize({
     dialect: 'sqlite',
     storage: './Bawasa.sqlite3',
     define: {
         freezeTableName: true
-    }
+    },
+    logging: false
 });
 
 module.exports = { db }
