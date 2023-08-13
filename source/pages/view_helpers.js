17f565021270e435d1e5fe1c1f28fcdaed922a92
AceBurgundy <Samadriansabalo99@gmail.com>
2023-08-13 22:43:38 +0800

    moved tryCatchWrapper to its own file.

---
+++ source/pages/view_helpers.js
@@ -0,0 +1,15 @@
+/**
+ * Wraps a callback function in a try-catch block for error handling.
+ * @function
+ * @param {Function} callback - The callback function to wrap.
+ */
+async function tryCatchWrapper(callback) {
+	try {
+		return await callback()
+	} catch (error) {
+		console.log(`\n\n${error.name}\n`)
+		console.log(`${error.message}`)
+	}
+}
+
+module.exports = tryCatchWrapper
