e397ba6c8d7fb5418a06166e3c1ef7f031115689
AceBurgundy <Samadriansabalo99@gmail.com>
2023-08-22 21:54:29 +0800

    Added new method addObject()

--- source/IPCResponse.js
+++ source/IPCResponse.js
@@ -13,43 +13,43 @@ class Response {
 	}
 
 	/**
-     * Sets the response status to "failed".
-     * @function
-     * @returns {Response} The Response instance.
-     */
+	 * Sets the response status to "failed".
+	 * @function
+	 * @returns {Response} The Response instance.
+	 */
 	failed() {
 		this.response.status = "failed";
 		return this;
 	}
 
 	/**
-     * Sets the response status to "success".
-     * @function
-     * @returns {Response} The Response instance.
-     */
+	 * Sets the response status to "success".
+	 * @function
+	 * @returns {Response} The Response instance.
+	 */
 	success() {
 		this.response.status = "success";
 		return this;
 	}
 
 	/**
-     * Adds a toast message to the response.
-     * @function
-     * @param {string} message - The message to be added.
-     * @returns {Response} The Response instance.
-     */
+	 * Adds a toast message to the response.
+	 * @function
+	 * @param {string} message - The message to be added.
+	 * @returns {Response} The Response instance.
+	 */
 	addToast(message) {
 		this.response.toast.push(message);
 		return this;
 	}
 
-    /**
-     * Adds a field-specific error message to the response.
-     * @function
-     * @param {string} fieldName - The name of the field associated with the error.
-     * @param {string} errorMessage - The error message to be added.
-     * @returns {Response} The Response instance.
-     */
+	/**
+	 * Adds a field-specific error message to the response.
+	 * @function
+	 * @param {string} fieldName - The name of the field associated with the error.
+	 * @param {string} errorMessage - The error message to be added.
+	 * @returns {Response} The Response instance.
+	 */
 	addFieldError(fieldName, errorMessage) {
 		if (!this.response.fieldErrors[fieldName]) {
 			this.response.fieldErrors[fieldName] = [];
@@ -58,25 +58,37 @@ class Response {
 		return this;
 	}
 
-    /**
-     * Sets the response status to "failed", adds toast and field error if provided.
-     * @function
-     * @param {string} [errorMessage] - The general error message to be added.
-     * @param {string} [fieldName] - The name of the field associated with the error.
-     * @returns {Object} The constructed response object.
-     */
+	/**
+	 * Adds or updates a key-value pair in the response object.
+	 * @function
+	 * @param {string} key - The key to be added or updated.
+	 * @param {any} value - The value associated with the key.
+	 * @returns {Response} The Response instance.
+	 */
+	addObject(key, value) {
+		this.response[key] = value;
+		return this;
+	}
+
+	/**
+	 * Sets the response status to "failed", adds toast and field error if provided.
+	 * @function
+	 * @param {string} [errorMessage] - The general error message to be added.
+	 * @param {string} [fieldName] - The name of the field associated with the error.
+	 * @returns {Object} The constructed response object.
+	 */
 	responseError(errorMessage, fieldName) {
 		this.failed();
 		errorMessage && this.addToast(errorMessage);
 		fieldName && this.addFieldError(fieldName, errorMessage);
 		return this.getResponse();
-	}	  
+	}
 
 	/**
-     * Retrieves the constructed response object.
-     * @function
-     * @returns {Object} The constructed response object.
-     */
+	 * Retrieves the constructed response object.
+	 * @function
+	 * @returns {Object} The constructed response object.
+	 */
 	getResponse() {
 		return this.response;
 	}
