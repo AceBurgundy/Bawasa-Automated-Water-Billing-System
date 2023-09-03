539a12219d0caf8fc98df103e3006c5cb63d0b84
AceBurgundy <Samadriansabalo99@gmail.com>
2023-09-03 10:37:42 +0800

    Fixed improper query ordering.

--- source/pages/clients/views.js
+++ source/pages/clients/views.js
@@ -1,38 +1,61 @@
-const Client_Connection_Status = require("../../../models/Client_Connection_Status");
-const ClientPhoneNumber = require("../../../models/Client_Phone_Number");
-const Client_Address = require("../../../models/Client_Address");
-const { db } = require("../../../sequelize_init");
-const Client = require("../../../models/Client");
-const { ipcMain } = require("electron");
+const Client_Connection_Status = require("../../../models/Client_Connection_Status")
+const ClientPhoneNumber = require("../../../models/Client_Phone_Number")
+const Client_Address = require("../../../models/Client_Address")
+const tryCatchWrapper = require("../view_helpers")
+const Client = require("../../../models/Client")
+const Response = require("../../IPCResponse")
+const { ipcMain } = require("electron")
 
 ipcMain.handle("clients", async (event, args) => {
 
-    try {
+    return tryCatchWrapper(async () => {
+
+        const response = new Response()
+
         const clients = await Client.findAll({
-			include: [
+		
+            include: [
 				{ 
                     model: ClientPhoneNumber, 
                     as: "Client_Phone_Numbers",
                     attributes: ['phoneNumber']
                 },
-                { model: Client_Address, as: "mainAddress" },
-				{ model: Client_Address, as: "presentAddress" },
+                { 
+                    model: Client_Address, 
+                    as: "mainAddress"
+                },
+				{ 
+                    model: Client_Address, 
+                    as: "presentAddress"
+                },
                 { 
                     model: Client_Connection_Status, 
                     as: "Client_Connection_Statuses",
                     attributes: ['status']
                 }
 			],
-		});
+            order: [
+                [
+                    {
+                        model: ClientPhoneNumber, 
+                        as: "Client_Phone_Numbers"
+                    },
+                    'createdAt', 'DESC'
+                ],
+                [
+                    {
+                        model: Client_Connection_Status, 
+                        as: "Client_Connection_Statuses"
+                    },
+                    'createdAt', 'DESC'
+                ]
+            ]
+		})
 
         if (clients.length > 0) {
-
-            return { status: "success", data: JSON.stringify(clients) }
+            return response.success().addObject("data", JSON.stringify(clients)).getResponse()
         } else {
-            return { status: "error", message: "No clients yet" }
+            return response.failed().addObject("message", "No clients yet").getResponse()
         }
-    } catch (error) {
-        console.error("Error fetching client data:", error)
-        throw error
-    }
-});
+    })
+})
