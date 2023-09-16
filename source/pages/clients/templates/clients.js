4e6d822f729ca6034f5afb01bb998d829818c537
AceBurgundy <Samadriansabalo99@gmail.com>
2023-09-16 23:57:57 +0800

    updated classes.

--- source/pages/clients/templates/clients.js
+++ source/pages/clients/templates/clients.js
@@ -46,21 +46,21 @@ export function clientTable(user, clients, responseMessage) {
         </nav>
 
         <section>
-            <div id="clients-section" class="section-child">
-                <div class="section-child__top">
+            <div id="clients-section" class="content">
+                <div class="content__top">
                     <div>
                         <img src="assets/images/Logo.png" alt="">
-                        <p class="section-child__top-title">${user ? `Welcome, ${user.firstName}` : `Welcome User`}</p>
+                        <p class="content__top-title">${user ? `Welcome, ${user.firstName}` : `Welcome User`}</p>
                     </div>
                     <img src="assets/images/Logo.png" alt="">
                 </div>
-                <div class="section-child__center">
-                    <div class="section-child__center-left">
-                        <p class="section-child__center-left__section-title">Clients</p>
-                        <p class="section-child__center-left__section-description">Check the latest reports and updates</p>
+                <div class="content__center">
+                    <div class="content__center-left">
+                        <p class="content__center-left__section-title">Clients</p>
+                        <p class="content__center-left__section-description">Check the latest reports and updates</p>
                     </div>
-                    <div class="section-child__center-right">
-                        <div class="section-child__center-right__options">
+                    <div class="content__center-right">
+                        <div class="content__center-right__options">
                             <button class="button-primary" id="client-options-toggle">Options</button>
                             <div id="client-options-toggle-options-list">
                                 <button class="button-primary" id="new-connection">
@@ -73,7 +73,7 @@ export function clientTable(user, clients, responseMessage) {
                         </div>
                     </div>
                 </div>
-                <div class="section-child__bottom">
+                <div class="content__bottom">
                     <div id="table-data">
                         <div id="table-data-title"><p>Clients</p></div>
                         <div id="table-data-headers">
