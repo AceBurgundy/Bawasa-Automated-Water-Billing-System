144438760794774b38733f464eb778eaa2d067cd
AceBurgundy <Samadriansabalo99@gmail.com>
2023-09-23 23:38:22 +0800

    removed most functionality and placed them on their respective classes.

--- source/pages/clientBuilder/static/clientBuilder.js
+++ source/pages/clientBuilder/static/clientBuilder.js
@@ -1,9 +1,10 @@
-// @collapse
 
+import DocumentBoard from "../../../assets/scripts/classes/DocumentBoard.js"
+import InputCapture from "../../../assets/scripts/classes/InputCapture.js"
 import { renderBillingSection } from "../../billing/static/billing.js"
 import { renderClientSection } from "../../clients/static/clients.js"
+import { renderProfile } from "../../profile/static/profile.js"
 import { getTemplate } from "../templates/clientBuilder.js"
-import Webcam from "../../../assets/scripts/Webcam.js"
 import "../../../utilities/constants.js"
 
 import { 
@@ -11,7 +12,10 @@ import {
 	transition,
 	getById,
 	queryElement,
-	queryElements
+	queryElements,
+	getFormData,
+	camelToDashed,
+	toSentenceCase
 } from "../../../assets/scripts/helper.js"
 
 /**
@@ -29,177 +33,34 @@ export async function renderClientBuilder(edit, clientObject) {
 		forEdit = edit
 	}
 
-    const formDataBuffer = {
-		formData: null,
-		image: null,
-		files: null
-	}
-
 	getById("container").innerHTML += getTemplate(forEdit, clientData)
 
-	setTimeout(() => {getById("section-type-container").classList.add("active")}, 500)
+	setTimeout(() => { getById("section-type-container").classList.add("active") }, 500)
+	
+	const capture = new InputCapture("clientProfile", forEdit, clientData?.profilePicture )
+	queryElement(".content__form-box__group__left").parentElement.innerHTML += capture
 
-	const canvas = getById("client-form-image-template")
-	const camera = getById("client-form-video")
-	const webcam = new Webcam(camera, "user", canvas)
-    
-    //if webcam is allowed, renders the capture toggle, else renders an file image input
-	webcam.info().then(data => {
-		const numberOfWebCams = data.filter(value => value["kind"] === "videoinput" && value["label"] !== "screen-capture-recorder").length
-		showHideImageCapture(numberOfWebCams)
-	})
-
-	//loads clients profile picture when form is set to edit
-	if (forEdit && clientData) {
-        const image = new Image()
-        const ctx = canvas.getContext('2d')
-		const imagePath = await window.ipcRenderer.invoke("get-client-image-path", clientData.profilePicture)
-        image.src = imagePath
-        
-        image.onload = function () {
-            canvas.width = image.width
-            canvas.height = image.height
-            ctx.drawImage(image, 0, 0)
-        }
-
-		formDataBuffer.image = {
-			base64: null,
-			fromInput: true,
-			path: imagePath,
-			size: null,
-			type: null,
-			format: clientData.profilePicture.split(".")[1],
-		}
-    } 
-
-	const input = getById('client-files-input')
-	const uploadedFiles = [];
-
-	const updateInputFiles = (uploadedFiles, input, doNotDisplayThumbnails = false) => {
-		const dataTransfer = new DataTransfer();
-		uploadedFiles.forEach(uploadedFile => dataTransfer.items.add(uploadedFile));
-		input.files = dataTransfer.files;
-		
-		if (doNotDisplayThumbnails) {
-			return
-		} else {
-			displayThumbnail(input.files)
-		}
-	} 
-
-	window.addEventListener('dragover', (event) => {
-        event.preventDefault()
-    })
-
-	window.addEventListener('drop', event => {
-
-		const droppedElement = event.target
-
-		if (droppedElement.classList.contains("client-form-files__box")) {
-
-			const files = event.dataTransfer.files
-
-			const uploadedFilesNames = uploadedFiles.map(file => file.name)
-			const fileNames = Array.from(files).map(file => file.name)
-
-			fileNames.forEach((name, index) => {
-				if (!uploadedFilesNames.includes(name)) {
-					uploadedFiles.push(files[index])
-				} else {
-					makeToastNotification(`${name} already exists`)
-				}
-			})
-			updateInputFiles(uploadedFiles, input)
-		}
-
-    })
-
-	input.addEventListener('change', async () => {
-		[...input.files].forEach(file => uploadedFiles.push(file))
-		displayThumbnail(uploadedFiles)
-    })
+	const fileCapture = new DocumentBoard("clientFiles", "Client Documents", forEdit)
+	queryElement(".files").innerHTML = fileCapture
 
 	window.onclick = event => {
 		
 		const { target } = event
 		const elementId = target.id
 
-		if (elementId === "billing") {
-			transition(renderBillingSection)
-		}
-
-		if (elementId === "clients") {
-			transition(renderClientSection)
-		}
-
-		if (target.classList.contains("client-form-files-box-preview__delete")) {
-
-			event.preventDefault()
-
-			const fileName = target.getAttribute("data-file-name")
-		
-			uploadedFiles.forEach((file, index) => {
-				if (file.name === fileName) uploadedFiles.splice(index, 1)
-			})
-
-			if (uploadedFiles.length === 0) {
-				getById("client-form-files-box-message").style.display = "flex"
-			}
-			
-			updateInputFiles(uploadedFiles, input, true)
-
-			target.parentElement.remove()
-
-		}
+		if (elementId === "billing") transition(renderBillingSection)
+		if (elementId === "clients") transition(renderClientSection)
+		if (elementId === "profile") transition(renderProfile)
 
 	}
 
 	// Handle form submission
 	getById("client-register-submit-button").addEventListener("click", event => {
 		event.preventDefault()
-        formDataBuffer.formData = new FormData(getById("client-form"))
 		if (forEdit && clientData !== null) {
-			handleFormSubmit(formDataBuffer, forEdit, clientData.id)
+			handleFormSubmit(forEdit, clientData.id)
 		} else {
-			handleFormSubmit(formDataBuffer)
-		}
-	})
-
-	// Handle image capture
-	getById("client-form-image-capture").addEventListener("click", event => {
-		event.preventDefault()
-
-		const { target } = event
-        const { classList } = target
-
-		const toggleCaptureClass = () => {
-			const newClass = classList.contains("take-image") ? "capture" : "take-image"
-			classList.replace("take-image", newClass)
-			target.innerHTML = newClass === "capture" ? "Capture" : "Take Image"
-			camera.style.zIndex = newClass === "capture" ? "2" : "1"
-			canvas.style.zIndex = newClass === "capture" ? "1" : "2"
-		}
-
-		if (classList.contains("take-image")) {
-			webcam.start()
-				.then(() => makeToastNotification("Click capture to capture the image"))
-				.catch(error => {
-					if (error === "Camera access denied") {
-						makeToastNotification(error)
-						getById("client-form-image").style.display = "block"
-						target.nextElementSibling.style.display = "none"
-					}
-				})
-			toggleCaptureClass()
-        }
-        
-        if (classList.contains("capture")) {
-			webcam.snap(data => {
-				formDataBuffer.image = { base64: data, fromInput: false }
-				canvas.value = data
-			})
-			webcam.stop()
-			toggleCaptureClass()
+			handleFormSubmit()
 		}
 	})
 
@@ -229,204 +90,38 @@ export async function renderClientBuilder(edit, clientObject) {
 			queryElement(`input[name='${targetName}']`).value = target.value
 		}
 	})
-    
-	getById("client-form-image").addEventListener("change", event => {
 
-        const fileInput = getById("client-form-image")
+	async function handleFormSubmit(forEdit = false, clientId = null) {
 
-		const ctx = canvas.getContext("2d")
+		const form = getById("client-form")
+		const formData = getFormData(form)
 
-		if (fileInput.files && fileInput.files[0]) {
-			const file = fileInput.files[0]
+		const invalidElements = queryElements(".invalid")
 
-			if (file.type.startsWith("image/")) {
-				const reader = new FileReader()
+		if (invalidElements.length > 0) return makeToastNotification("Fix errors first")
 
-				reader.onload = function (e) {
-					const image = new Image()
-
-					image.onload = function () {
-						canvas.width = image.width
-						canvas.height = image.height
-						ctx.drawImage(image, 0, 0)
-					}
-
-					image.src = e.target.result
-
-					formDataBuffer.image = {
-						base64: null,
-						fromInput: true,
-						path: file.path,
-						size: file.size,
-						type: file.type,
-						format: file.name.split(".")[1],
-					}
-				}
-
-				reader.readAsDataURL(file)
-			} else {
-				event.preventDefault()
-				return "Please select an image file"
-			}
-		}
-	})
-
-	function showHideImageCapture(numWebcams) {
-		const registrationImage = getById("client-form-image")
-		const imageCapture = getById("client-form-image-capture")
-
-		if (numWebcams > 0) {
-			registrationImage.style.display = "none"
-			imageCapture.style.display = "block"
-		} else {
-			registrationImage.style.display = "block"
-			imageCapture.style.display = "none"
-		}
-	}
-
-	function addFieldError(field, error) {
-		const errorField = queryElement(`ul[data-error-key="${field}"]`)
-
-		if (errorField) {
-			errorField.innerHTML = `<li class="client-form-input-box__title__errors-item">${error}</li>`
-		}
-	}
-
-	function clearFieldErrors() {
-		const fieldErrors = queryElements(".client-form-input-box__title__errors-item")
-
-		if (fieldErrors) {
-			fieldErrors.forEach(field => (field.innerHTML = ""))
-		}
-	}
-
-	function validateFormData(formDataBuffer) {
-
-        clearFieldErrors()
-
-		let errors = 0
-
-		const longestRelationshipOption = Object.values(window.userRelationshipTypes).reduce((a, b) => (b.length > a.length ? b : a)).length
-		const shortestRelationshipOption = Object.values(window.userRelationshipTypes).reduce((a, b) => (b.length < a.length ? b : a)).length
-
-		const validationMethods = {
-			
-			firstName: [[window.isEmpty], [window.isOverThan, 2, 255]],
-
-			middleName: [[window.isEmpty], [window.isOverThan, 2, 255]],
-
-			lastName: [[window.isEmpty], [window.isOverThan, 2, 255]],
-
-			relationshipStatus: [[window.isEmpty], [window.isOverThan, shortestRelationshipOption, longestRelationshipOption], [window.notIn, [...Object.values(window.userRelationshipTypes)]]],
-
-			birthDate: [[window.isEmpty], [window.isBirthDate]],
-
-			age: [[window.isEmpty], [window.isOverThan, 15, 70]],
-
-			email: [[window.isEmpty], [window.isEmail], [window.isOverThan, 10, 255]],
-
-			occupation: [[window.isEmpty], [window.isOverThan, 5, 255]],
-
-			phoneNumber: [[window.isEmpty], [window.isValidPhoneNumber]],
-
-			presentAddressStreet: [[window.isEmpty], [window.isOverThan, 5, 9999]],
-
-			presentAddressSubdivision: [[window.isEmpty], [window.isOverThan, 5, 255]],
-
-			presentAddressBarangay: [[window.isEmpty], [window.isOverThan, 5, 255]],
-
-			presentAddressCity: [[window.isEmpty], [window.isOverThan, 5, 255]],
-
-			presentAddressProvince: [[window.isEmpty], [window.isOverThan, 5, 255]],
-
-			presentAddressPostalCode: [[window.isEmpty], [window.isOverThan, 5, 9999]],
-
-			presentAddressDetails: [[window.isEmpty], [window.isOverThan, 5, 255]],
-
-			mainAddressStreet: [[window.isEmpty], [window.isOverThan, 5, 9999]],
-
-			mainAddressSubdivision: [[window.isEmpty], [window.isOverThan, 5, 255]],
-
-			mainAddressBarangay: [[window.isEmpty], [window.isOverThan, 5, 255]],
-
-			mainAddressCity: [[window.isEmpty], [window.isOverThan, 5, 255]],
-
-			mainAddressProvince: [[window.isEmpty], [window.isOverThan, 5, 255]],
-
-			mainAddressPostalCode: [[window.isEmpty], [window.isOverThan, 5, 9999]],
-
-			mainAddressDetails: [[window.isEmpty], [window.isOverThan, 5, 255]],
-		}
-
-        formDataBuffer.formData.forEach((dirtyValue, key) => {
-			if (typeof dirtyValue !== "object") {
-				const value = dirtyValue.trim()
-
-				if (validationMethods.hasOwnProperty(key)) {
-					validationMethods[key].forEach(([validationMethod, ...args]) => {
-						const [validationErrors, validationMessage] = validationMethod(value, ...args)
-						errors += validationErrors
-
-						validationMessage.length > 0 && addFieldError(key, validationMessage)
-					})
-				}
-			}
-		})
-
-        if (formDataBuffer.image === null) {
-			makeToastNotification("Profile Picture is required when registering a new client")
-			errors++
-		}
-
-		return errors
-	}
-
-	async function handleFormSubmit(formDataBuffer, forEdit = false, clientId = null) {
-
-		clearFieldErrors()
-
-		// DSGKHOS;D update to check proper validate
-		// DSGJDS check for proper files.
-		const result = validateFormData(formDataBuffer.formData)
-
-		if (result.count > 0) {
-			const { field, message } = result
-			message.forEach(text => addFieldError(field, text))
-			return
-		}
-
-		const inputFiles = getById('client-files-input');
-
-		if (inputFiles) {
-		  const filesData = [];
-		  
-		  for (const file of inputFiles.files) {
-			filesData.push({
-				name: file.name,
-				size: file.size,
-				path: file.path
-			})
-		  }
+		const imageData = capture.imageData
+		const documentsArray = extractFileData(fileCapture.getFiles())
 		
-		  formDataBuffer.files = filesData;
-		}
-
-		//remove clientFiles from formData 
-		formDataBuffer.formData.delete("clientFiles")
-		formDataBuffer.formData = Object.fromEntries(formDataBuffer.formData.entries())
-
 		let response = null
 		
-		if (forEdit && clientId !== null) {
+		if (forEdit && clientId) {
 
 			response = await window.ipcRenderer.invoke("edit-client", {
-				formDataBuffer: formDataBuffer,
+				formDataBuffer: {
+					formData: formData,
+					profilePicture: imageData.image
+				},
 				clientId: clientId
 			})
 
 		} else {
 			
-			response = await window.ipcRenderer.invoke("add-client", formDataBuffer)
+			response = await window.ipcRenderer.invoke("add-client", {
+				formData: formData,
+				image: imageData.image,
+				files: documentsArray
+			})
 
 		}
 
@@ -437,108 +132,33 @@ export async function renderClientBuilder(edit, clientObject) {
 
 		} else {
 
-			if (response.field_errors) {
-				Object.keys(response.field_errors).forEach(key => {
-					queryElement(`ul[data-error-key="${key}"]`).innerHTML = `
-						<li class="client-form-input-box__title__errors-item">
-							${response.field_errors[key]}
-						</li>
-					`
+			if (response.fieldErrors) {
+				Object.entries(response.fieldErrors).forEach(([name, error]) => {
+					const dashedName = camelToDashed(name)
+					const cleanName = dashedName.includes("-") ? dashedName.split("-").join(" ") : dashedName
+					const cleanErrorMessage = toSentenceCase([cleanName, error[0]].join(" "))
+					getById(`${dashedName}-field__info__error`).textContent = error[0]
+					makeToastNotification(cleanErrorMessage)
 				})
 			}
 
-			response.toast.forEach(toast => {
-				makeToastNotification(toast)
-			})
+			response.toast.forEach(toast => makeToastNotification(toast))
 		}
 	}
 }
 
 /**
- * 
- * @param {String} fileType - the value of the key "type" from the File object 
- * @param {*} event - Event from the reader onload function
- * @returns String - path to the icon image file
+ * Extracts file data from a FileList object.
+ *
+ * @param {FileList | null} inputFiles - The FileList object containing selected files, or null if no files were selected.
+ * @returns {Array<Object>} An array of file data objects, each containing 'name', 'size', and 'path' properties.
  */
-async function getFileIcon(fileType, event) {
+const extractFileData = (inputFiles) => {
+    if (!inputFiles) return [];
 
-    const imagePath = async (fileName) => await window.ipcRenderer.invoke("get-file-image-path", fileName)
-    
-	if (fileType.includes("image/")) {
-        return event.target.result
-    } 
-	
-	if (fileType.includes("application/pdf")) {
-        return await imagePath("pdf-icon.PNG")
-    }
-	
-	if (fileType.includes("application/vnd.openxmlformats-officedocument.wordprocessingml.document")) {
-        return await imagePath("docx-icon.PNG")
-    }
-	
-	if (fileType.includes("application/msword")) {
-        return await imagePath("doc-icon.PNG")
-    }
-	
-	if (fileType.includes("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") ||
-        fileType.includes("application/vnd.ms-excel") ||
-        fileType.includes("text/csv")) {
-        return await imagePath("sheet-icon.PNG")
-    }
-
-	return await imagePath("default-icon.png")
-
-}
-
-/**
- * Adds the thumbnails of the added images preview
- * @param {*} file - File object
- */
-function displayThumbnail(files) {
-
-	const addNewFileMessage = getById("client-form-files-box-message")
-	const computedStyle = window.getComputedStyle(addNewFileMessage);
-	const displayPropertyValue = computedStyle.getPropertyValue("display");
-
-	if (displayPropertyValue === "flex") addNewFileMessage.style.display = "none"
-
-	Array.from(files).forEach(file => {
-
-		const previewExists = queryElement(`[data-preview-file-name="${file.name}"]`)
-		if (previewExists) return
-
-		const addNewFilePreview = (image, fileName) => {
-
-			const div = document.createElement("div")
-			div.className = "client-form-files-box-preview"
-			div.setAttribute("data-preview-file-name", fileName)
-			
-			const deleteButton = document.createElement("div")
-			deleteButton.className = "client-form-files-box-preview__delete"
-			deleteButton.setAttribute("data-file-name", fileName)
-			deleteButton.textContent = "Remove"
-	
-			const p = document.createElement("p")
-			p.className = "client-form-files-box-preview__text"
-			p.textContent = fileName
-	
-			div.appendChild(deleteButton)
-			div.appendChild(image)
-			div.appendChild(p)
-			return div
-		}
-	
-		const reader = new FileReader()
-		reader.onload = async function(event) {
-			const img = document.createElement('img')
-			img.className = "client-form-files-box-preview__image"
-			img.src = await getFileIcon(file.type, event)
-			img.alt = file.name
-			const imagePreview = addNewFilePreview(img, file.name)
-			queryElement(".client-form-files__box").appendChild(imagePreview)
-		}
-		reader.readAsDataURL(file)
-
-	})
-
-}
+    return Array.from(inputFiles).map((file) => ({
+        name: file.name,
+        size: file.size,
+        path: file.path
+    }));
+};
