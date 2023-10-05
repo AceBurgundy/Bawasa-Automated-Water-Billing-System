4e9fddca737fc95319bddf923f3a69b750bd1820
AceBurgundy <Samadriansabalo99@gmail.com>
2023-10-05 23:03:10 +0800

    added new function formatDate and generateNextAccountOrBillNumber.

--- source/utilities/helpers.js
+++ source/utilities/helpers.js
@@ -13,4 +13,54 @@ async function tryCatchWrapper(callback) {
 	}
 }
 
-module.exports = tryCatchWrapper
+function formatDate(date) {
+    return date ?? false ? new Date(date).toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+        })    
+    : ''
+}
+
+/**
+ * Generates the next account/bill number based on the last client's account/bill number.
+ * @async
+ * @function
+ * @returns {Promise<string>} The generated account number.
+ */
+const generateNextAccountOrBillNumber = async function (type) {
+
+	isClient = type === "Client"
+
+    const latestRecord = await isClient ?
+		await Client.findOne({ order: [["createdAt", "DESC"]] })
+	:
+		await ClientBill.findOne({ order: [["createdAt", "DESC"]] })
+
+    if (!latestRecord) {
+        return isClient ? "0000-AA" : "000000000-AAAA"
+    }
+
+    let nextNumber = isClient ? "0000" : "000000000"
+    let nextLetter = isClient ? "AA" : "AAAA"
+
+    const latestNumber = isClient ? latestRecord.accountNumber : latestRecord.billNumber
+    const numberSection = parseInt(latestNumber.split("-")[0], 10)
+    const letterSection = latestNumber.split("-")[1]
+
+    if (numberSection === 9999 || numberSection === 999999999) {
+        const lastLetterCharCode = letterSection.charCodeAt(letterSection.length - 1);
+        
+        lastLetterCharCode === 90
+            ? (nextLetter = isClient ? "AA" : "AAAA")
+            : (nextLetter = "A" + String.fromCharCode(lastLetterCharCode + 1))
+    } else {
+        nextNumber = String((isClient ? "0000" : "000000000") + (numberSection + 1)).slice(-nextNumber.length)
+        nextLetter = letterSection
+    }
+
+	return `${nextNumber}-${nextLetter}`
+
+}
+
+module.exports = { tryCatchWrapper, formatDate, generateNextAccountOrBillNumber }
