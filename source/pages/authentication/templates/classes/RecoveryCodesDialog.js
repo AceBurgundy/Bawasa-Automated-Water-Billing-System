14eda8a827e6d45af94023ac03eb4994a95eddb1
AceBurgundy <Samadriansabalo99@gmail.com>
2023-10-06 22:30:20 +0800

    moved header at the top.

--- source/pages/authentication/templates/classes/RecoveryCodesDialog.js
+++ source/pages/authentication/templates/classes/RecoveryCodesDialog.js
@@ -21,16 +21,16 @@ export default class RecoveryCodesDialog {
         
         this.template = `
             <form class="recovery-codes-form">
-                <p class="recovery-codes-form__title">${title}</p>
+                <p class="recovery-codes-form__title">Recovery Codes</p>
 
                 <div class="recovery-codes-form__center">
-                    <p class="recovery-codes-form__center__warning">This will be the recovery codes for your account. Without a recovery code you will never be able to recover your account. Click the icon to copy it and save it somewhere safe.</p>
-                    <div class="recovery-codes-form__center__list">
-                        <div class="recovery-codes-form__center__list__header">
-                            <div id="${ this.clipBoardBoxId }">
-                                <svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 32 32" id="clipboard"><path d="M24,30H8a3,3,0,0,1-3-3V7A3,3,0,0,1,8,4h2a1,1,0,0,1,0,2H8A1,1,0,0,0,7,7V27a1,1,0,0,0,1,1H24a1,1,0,0,0,1-1V7a1,1,0,0,0-1-1H22a1,1,0,0,1,0-2h2a3,3,0,0,1,3,3V27A3,3,0,0,1,24,30Z"></path><path d="M22,8H10A1,1,0,0,1,9,7V5a3,3,0,0,1,3-3h1.17a3,3,0,0,1,5.66,0H20a3,3,0,0,1,3,3V7A1,1,0,0,1,22,8ZM11,6H21V5a1,1,0,0,0-1-1H18a1,1,0,0,1-1-1,1,1,0,0,0-2,0,1,1,0,0,1-1,1H12a1,1,0,0,0-1,1Zm8-3h0Z"></path></svg>
-                            </div>
+                    <div class="recovery-codes-form__center__list__header">
+                        <p class="recovery-codes-form__center__warning">This will be the recovery codes for your account. Without a recovery code you will never be able to recover your account. Click the icon to copy it and save it somewhere safe.</p>
+                        <div id="${ this.clipBoardBoxId }" class="recovery-codes-form__center__list__header__clipboard-box">
+                            <svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 32 32" id="clipboard"><path d="M24,30H8a3,3,0,0,1-3-3V7A3,3,0,0,1,8,4h2a1,1,0,0,1,0,2H8A1,1,0,0,0,7,7V27a1,1,0,0,0,1,1H24a1,1,0,0,0,1-1V7a1,1,0,0,0-1-1H22a1,1,0,0,1,0-2h2a3,3,0,0,1,3,3V27A3,3,0,0,1,24,30Z"></path><path d="M22,8H10A1,1,0,0,1,9,7V5a3,3,0,0,1,3-3h1.17a3,3,0,0,1,5.66,0H20a3,3,0,0,1,3,3V7A1,1,0,0,1,22,8ZM11,6H21V5a1,1,0,0,0-1-1H18a1,1,0,0,1-1-1,1,1,0,0,0-2,0,1,1,0,0,1-1,1H12a1,1,0,0,0-1,1Zm8-3h0Z"></path></svg>
                         </div>
+                    </div>
+                    <div class="recovery-codes-form__center__list">
                         <div class="recovery-codes-form__center__list__codes">
                             ${ recoveryCodeElements.join("") }
                         </div>
@@ -48,6 +48,7 @@ export default class RecoveryCodesDialog {
     }
 
     toString() {
+        console.log(this.template);
         fillAndShowDialog(this.template)
     }
 
