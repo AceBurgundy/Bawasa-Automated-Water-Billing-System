e97675d75922bb82837aab77a747fba67170c3b5
AceBurgundy <Samadriansabalo99@gmail.com>
2023-11-26 10:40:45 +0800

    added jsDoc to session

--- source/utilities/session.js
+++ source/utilities/session.js
@@ -6,25 +6,51 @@ const User = require("../../models/User")
 
 const SESSION_KEY = "user_session"
 
+/**
+ * Manages user sessions using Electron Store.
+ * @private
+ * @class
+ */
 class SessionManager {
 
+    /**
+     * Creates an instance of SessionManager.
+     * @constructor
+     */
     constructor() {
         this.store = new Store()
     }
 
+    /**
+     * Logs in a user by storing the access key in the session.
+     * @method
+     * @param {string} accessKey - The access key associated with the user.
+     */
     login(accessKey) {
         this.store.set(SESSION_KEY, accessKey)
     }
 
+    /**
+     * Logs out the current user by clearing the session.
+     * @method
+     */
     logout() {        
         this.store.clear()
     }
 
+    /**
+     * Retrieves the current user based on the stored access key.
+     * @async
+     * @method
+     * @throws {Error} Throws an error if there is an issue retrieving the user information.
+     * @returns {Promise<Object|null>} A Promise that resolves to the user object or null if the access key is not found.
+     */
     async current_user() {
+        
         const accessKey = this.store.get(SESSION_KEY)
 
         if (!accessKey) {
-            console.log("\n\nAccess key not found\n\n");
+            console.log("Access key not found");
             return null
         }
 
@@ -60,20 +86,21 @@ class SessionManager {
                 ],
             })
             
-            if (user) {
-                return user.toJSON()
-            } else {
-                return null
-            }
+            return user ? user.toJSON() : null
 
         } catch(error) {
             console.log(error);
+            return null
         }         
 
     }
 
 }
 
+/**
+ * Default instance of the SessionManager class.
+ * @const {Object}
+ */
 const session = new SessionManager()
 
 module.exports = session
