ae835e18b4f29d6a4fdf75393d8f09c7f1ebdff6
AceBurgundy <Samadriansabalo99@gmail.com>
2023-12-13 14:32:38 +0800

    Added paths for icon.

--- forge.config.js
+++ forge.config.js
@@ -1,30 +1,39 @@
+/* eslint-disable linebreak-style */
 module.exports = {
   packagerConfig: {
     asar: true,
- },
+    icon: './source/assets/images/Logo.png'
+  },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
-   },
+      config: {
+        iconUrl: 'https://drive.google.com/uc?export=download&id=11MSw42Soj7puz7jhhQkwEGLfx9uGveIH',
+        setupIcon: './source/assets/images/Logo.ico'
+      }
+    },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-   },
+      platforms: ['darwin']
+    },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
-   },
+      config: {
+        icon: './source/assets/images/Logo.png'
+      }
+    },
     {
       name: '@electron-forge/maker-rpm',
-      config: {},
-   },
+      config: {
+        icon: './source/assets/images/Logo.png'
+      }
+    }
   ],
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-   },
-  ],
+      config: {}
+    }
+  ]
 };
