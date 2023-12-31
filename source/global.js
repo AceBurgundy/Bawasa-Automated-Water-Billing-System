f53e8619e8cbf7a3afe40bef2e1ae44212081c9c
AceBurgundy <Samadriansabalo99@gmail.com>
2023-12-31 21:00:58 +0800

    added renderer for showing error message from console.

--- source/global.js
+++ source/global.js
@@ -3,3 +3,7 @@ import makeToastNotification from './assets/scripts/toast.js';
 window.ipcRenderer.on('toast', (event, message) => {
   makeToastNotification(message);
 });
+
+window.ipcRenderer.on('console', (event, message) => {
+  console.log(`Message from server:\n\n${message}`);
+});
